a39d413ff8f0f9924361d412d3cd276251556e56
David Vasconcellos <85040759+davidvasconcellos@users.noreply.github.com>
2025-08-21 14:39:14 -0300

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
+/*!
+ * Autor: David Vasconcellos
+ * Email: davidvasconcellos16@gmail.com
+ * Direitos autorais reservados - 2025
+ */
 // Script executável em todas as páginas para verificar se contém o URL do sistema de contracheque e enviar mensagem para o popup (se ele estiver aberto).
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg === "verificarURL") {
